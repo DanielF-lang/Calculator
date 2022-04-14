e7425a6d6d4b8380c559c37b6cf9709935d17b3e
Daniel Fagyas <daniel.fagyas26@gmail.com>
2022-04-14 09:20:28 +0100

    minor changes

--- main.c
+++ main.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(int argc, char* argv[]) {
     int number1, number2;
-    printf("Enter First and Second Numbers: ");
-    scanf("%i %i", &number1, &number2);
+    int operator;
+
+    printf("Enter First Value: ");
+    scanf("%i", &number1);
 
     system("cls");
 
-    int operator;
-    printf("Select Valid Operator:\r\n1-Addition\r\n2-Subtraction\r\n3-Multiplication\r\n4-Division\r\n5-Remainder\r\nEnter Operator: ");
+    printf("Operators:\r\n1-Addition\r\n2-Subtraction\r\n3-Multiplication\r\n4-Division\r\n5-Modulo\r\nOperator:");
     scanf("%i", &operator);
+
+    system("cls");
+
+    printf("Enter Second Value: ");
+    scanf("%i", &number2);
 
     system("cls");
 
     switch(operator) {
         case 1:
-        printf("%i Add %i is %i", number1, number2, (number1 + number2));
+        printf("%i add %i is %i", number1, number2, (number1 + number2));
         break;
 
         case 2:
-        printf("%i Minus %i is %i", number1, number2, (number1 - number2));
+        printf("%i sub %i is %i", number1, number2, (number1 - number2));
         break;
-        
+
         case 3:
-        printf("%i Multiply %i is %i", number1, number2, (number1 * number2));
+        printf("%i mul %i is %i", number1, number2, (number1 * number2));
         break;
 
         case 4:
-        printf("%i Divide %i is %i", number1, number2, (number1 / number2));
+        printf("%i div %i is %f", number1, number2, ((float)number1 / (float)number2));
         break;
 
         case 5:
-        printf("%i Modulo %i is %i", number1, number2, (number1 % number2));
+        printf("%i mod %i is %i", number1, number1, (number1 % number2));
         break;
     }
 
